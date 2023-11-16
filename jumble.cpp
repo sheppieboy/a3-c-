ae1270cd8a0c8348b481a2f4223d347c04575e12
Luke Sheppard <sheppiegit@gmail.com>
2023-11-15 21:33:50 -0500

    added the JumblePuzzle class accessors.

--- jumble.cpp
+++ jumble.cpp
@@ -6,3 +6,27 @@ BadJumbleException::BadJumbleException(const string& message) : error_message(me
 string BadJumbleException::what() const {
     return error_message;
 }
+
+JumblePuzzle::JumblePuzzle(const string& hiddenWord, const string& difficulty){
+    
+}
+
+charArrayPtr* JumblePuzzle::getJumble() const {
+    return jumble;
+}
+
+int JumblePuzzle::getSize() const {
+    return size;
+}
+
+int JumblePuzzle::getRowPos() const {
+    return rowPos;
+}
+
+int JumblePuzzle::getColPos() const {
+    return colPos;
+}
+
+char JumblePuzzle::getDirection() const {
+    return direction;
+}
