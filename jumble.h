fe609dcf7d3dad3d479fe9ed2c92be6e5187befb
Luke Sheppard <sheppiegit@gmail.com>
2023-11-15 21:25:38 -0500

    added JumblePuzzle class in the header file.

--- jumble.h
+++ jumble.h
@@ -2,6 +2,7 @@
 #define JUMBLE_H
 
 #include <string>
+typedef char* charArrayPtr;
 
 class BadJumbleException {
 public: 
@@ -10,6 +11,37 @@ public:
 
 public:
     std::string error_message;
+};
+
+class JumblePuzzle {
+public:
+    //constructor
+    JumblePuzzle(const std::string& hiddenWord, const std::string& difficulty);
+
+    //copy constructor
+     JumblePuzzle(const JumblePuzzle& other);
+
+     //destructor, will free up and memory on the heap when removing from 
+    virtual ~JumblePuzzle();
+
+
+    //assignment operator for jumble puzzle class
+    JumblePuzzle& operator=(const JumblePuzzle& other);
+
+    //accessors
+    charArrayPtr* getJumble() const;
+    int getSize() const;
+    int getRowPos() const;
+    int getColPos() const;
+    char getDirection() const;
+
+private:
+    charArrayPtr* jumble;
+    int size;
+    int rowPos;
+    int colPos;
+    char direction;
+
 };
 
 #endif
